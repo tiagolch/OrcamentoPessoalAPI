348468cd591382f27422302383fd94603c65472b
tiagolch <tiagolch@hotmail.com>
2022-02-14 18:50:07 -0300

    Criado app orcamento e modelado banco receita e despesa
    Signed-off-by: tiagolch <tiagolch@hotmail.com>

---
+++ orcamentopessoal/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class OrcamentopessoalConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'orcamentopessoal'
---
+++ orcamentopessoal/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+
+class Receita(models.Model):
+    descricao = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+    def get_data(self):
+        return self.data.strftime('%d/%m/%Y')
+
+
+class Despesa(models.Model):
+    descricao = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data = models.DateField()
+
+    def __str__(self):
+        return self.nome
+
+    def get_data(self):
+        return self.data.strftime('%d/%m/%Y')
