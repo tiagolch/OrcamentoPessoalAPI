069e46691e826b385739c90ed293199dc904966c
m2b-tiagochaves <tiago.chaves@mobi2buy.com>
2022-04-11 16:51:52 -0300

    Adicionado novos molelos
    Signed-off-by: m2b-tiagochaves <tiago.chaves@mobi2buy.com>

--- orcamentopessoal/admin.py
+++ orcamentopessoal/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Receita, Despesa
+from .models import Receita, Despesa, Categoria, Pessoa, Cartao
+
+
+@admin.register(Despesa)
+class DespesaAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'valor', 'pessoa', 'get_data')
+    list_filter = ('data', 'categoria', 'pessoa', 'cartao')
+    search_fields = ('descricao',)
 
 @admin.register(Receita)
 class ReceitaAdmin(admin.ModelAdmin):
@@ -7,9 +14,25 @@ class ReceitaAdmin(admin.ModelAdmin):
     list_filter = ('data',)
     search_fields = ('descricao',)
 
-@admin.register(Despesa)
-class DespesaAdmin(admin.ModelAdmin):
-    list_display = ('descricao', 'valor', 'get_data')
-    list_filter = ('data',)
-    search_fields = ('descricao',)
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ativo')
+    list_filter = ('ativo',)
+    search_fields = ('nome',)
+
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'ativo')
+    list_filter = ('ativo',)
+    search_fields = ('nome',)
+
+
+@admin.register(Cartao)
+class CartaoAdmin(admin.ModelAdmin):
+    list_display = ('bandeira', 'numero', 'vencimento', 'get_data')
+    list_filter = ('vencimento',)
+    search_fields = ('bandeira', 'numero')
 
--- orcamentopessoal/models.py
+++ orcamentopessoal/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from django.db import models
 
 
@@ -13,10 +14,53 @@ class Receita(models.Model):
         return self.data.strftime('%d/%m/%Y')
 
 
+class Cartao(models.Model):
+    bandeira = models.CharField(max_length=100)
+    numero = models.PositiveIntegerField()
+    vencimento = models.DateField()
+    data = models.DateField(auto_now_add=True)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.bandeira
+
+    def get_vencimento(self):
+        return self.vencimento.strftime('%d')
+
+    def get_data(self):
+        return self.data.strftime('%d/%m/%Y')
+
+    class Meta:
+        verbose_name_plural = 'Cartões'
+
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    ativo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nome
+
+
 class Despesa(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+    pessoa = models.ForeignKey(Pessoa, on_delete=models.DO_NOTHING, null=True, blank=True)
     descricao = models.CharField(max_length=100)
     valor = models.DecimalField(max_digits=10, decimal_places=2)
-    data = models.DateField()
+    cartao = models.ForeignKey(Cartao, on_delete=models.DO_NOTHING, null=True, blank=True)
+    parcela = models.PositiveIntegerField(null=True, blank=True)
+    fixo = models.BooleanField(default=False)
+    pago = models.BooleanField(default=False)
+    data = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.descricao
